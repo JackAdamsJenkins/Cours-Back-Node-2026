3609930d53e1088164b5bbc94d76f1e021fd6194
Jean-Louis ERRANTE <clensx@gmail.com>
2026-07-23 11:54:49 +0200

    feat: add PostgreSQL support with Sequelize ORM and new user routes
    - Added DATABASE_URL to .env.local for PostgreSQL connection.
    - Integrated Sequelize and PostgreSQL dependencies (pg, pg-hstore, sequelize).
    - Configured database connection and table synchronization on startup.
    - Added new user route under /api/v2/users using Sequelize models.

--- app.js
+++ app.js
@@ -6,10 +6,20 @@ const port = 3000
 // Ensuite pour utiliser : process.env.NOM_DE_VARIABLE_DANS_ENV
 require('dotenv').config()
 require('./config/db')
+const { sequelize, connectDB } = require('./config/database')
+// Connexion à la BDD
+const startServer = async () => {
+    await connectDB()
+
+    // Créer les tables si elles n'existent pas
+    await sequelize.sync({ alter: false })
+    console.log('Tables synchronized')
+}
 // Import des routes
 const productsRoutes = require('./routes/productsRoutes')
 const authRoutes = require('./routes/authRoutes')
 const userRoutes = require('./routes/userRoutes')
+const userSeqRoutes = require('./routes/userSeqRoutes')
 
 app.use(express.json())
 
@@ -17,7 +27,9 @@ app.use(express.json())
 app.use('/api/v1/products', productsRoutes)
 app.use('/api/v1/auth', authRoutes)
 app.use('/api/v1/users', userRoutes)
+app.use('/api/v2/users', userSeqRoutes)
 
+startServer()
 //     URL
 app.get('/', (req, res) => {
     res.send('Bienvenue sur mon API RESTful !')
