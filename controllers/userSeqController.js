3291976811beff4ac88206ad9523b91970945b6b
Jean-Louis ERRANTE <clensx@gmail.com>
2026-07-23 12:13:12 +0200

    refactor(userSeqController): replace ORM findAll with raw SQL query

--- controllers/userSeqController.js
+++ controllers/userSeqController.js
@@ -1,9 +1,14 @@
 const User = require('../models/userSeqModel')
+const { QueryTypes} = require('sequelize')
+const { sequelize } = require('../config/database')
 
 // Get all users
 exports.getUsers = async (req, res) => {
     try {
-        const users = await User.findAll()
+        // const users = await User.findAll()
+        const users = await sequelize.query('SELECT * FROM "Users"', {
+            type: QueryTypes.SELECT
+        })
         res.json(users)
     } catch (err) {
         res.status(500).json({ error: err.message })
